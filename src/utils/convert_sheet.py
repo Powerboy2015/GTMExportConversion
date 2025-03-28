dd3d51a667f050ff88dff98fb204986ae6189bce
Powerboy2015 <zicobacker26@gmail.com>
2025-03-28 20:13:53 +0100

    Convert sheets finished I have now finished up the export to .csv file. Destination is still an issue, but will be fixed with next commit.

--- src/utils/convert_sheet.py
+++ src/utils/convert_sheet.py
@@ -24,22 +24,20 @@ def convert_to_csv(filepaths: list[str],destination: str ="") -> None:
             print(tag)
             sheet_obj.append(tag)
 
-
         sheet_obj.column_dimensions["a"].width = 45
         sheet_obj.column_dimensions["b"].width = 20
         sheet_obj.column_dimensions["c"].width = 20
-        sheet_obj.column_dimensions["d"].width = 25
+        sheet_obj.column_dimensions["d"].width = 40
         sheet_obj.column_dimensions["e"].width = 15
         sheet_obj.column_dimensions["f"].width = 10
 
         sheet_obj.freeze_panes = sheet_obj['A2']
 
-
         for cell in sheet_obj[1]:
             cell.font = Font(bold=True)
 
-
     wb.save("output.xlsx")
+
 
 def files_to_data(filepaths)->dict[str, dict]:
     data_sheets = {}
@@ -58,36 +56,18 @@ def files_to_data(filepaths)->dict[str, dict]:
     return data_sheets
 
 
-def process_tag(tag,triggers):
+def process_tag(tag: dict[str,str],triggers):
     event = confirm_event(tag['type'])
+    trigger_id = tag.get("firingTriggerId") or ""
+    print(tag['name'])
     return [
         tag['name'] or "",
         event or "",
         sends_to(event) or "",
-        get_trigger_name(tag['firingTriggerId'], triggers) or "",
+        get_trigger_name(trigger_id or "", triggers) or "",
         is_consent_needed(tag['consentSettings']['consentStatus']) or "",
         get_additional_info(tag) or ""
     ]
-
-# Outputs a file from imports to exports to use in a table.
-# def OutputToFile (fileName):
-#     data = FileToJson("./JSON_imports/"+fileName)
-#
-#     tags = data['containerVersion']['tag']
-#     triggers = data['containerVersion']['trigger']
-#
-#     print("Exporting table...")
-#     total = 0
-#     with open("./JSON_outputs/"+fileName.strip('.json')+"_SHEETABLE.txt", "w") as output:
-#         for tag in tags:
-#
-#             if 'firingTriggerId' not in tag:
-#                 tag['firingTriggerId'] = "All pages"
-#
-#             event = confirmEvent(tag['type'])
-#             output.write(f"{tag['name']}\t{event}\t{SendsTo(event)}\t{GetTriggerName(tag['firingTriggerId'],triggers)}\t{isConsentNeeded(tag['consentSettings']['consentStatus'])}\t{getAdditionalInfo(tag)}\t\n")
-#             total = total + 1
-#     print(f"done! {total} tags found and extracted.")
 
 
 def confirm_event(name):
@@ -128,6 +108,7 @@ def sends_to(category):
         return "Datalayer"
     else:
         return category
+
 
 def get_additional_info(tag):
     response = ""
